7d3afa29b11c602fa1e27abe18c8188e411dc282
Vishal Sharma <sharma.vishal2799@gmail.com>
2025-07-07 14:27:05 +0530

    tailwind, mantine, setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+// import App from './App.tsx'
+import { Button } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <MantineProvider>
+<div className="bg-amber-300 p-5"><Button className="bg-red-500 text-white">Hello</Button></div>
+</MantineProvider>
+    ),
+  },
+]);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
